958ed58cfd1c19654a7667f690396daec9125b3c
caionikolas <Caio.kanu@hotmail.com>
2022-07-19 22:47:54 -0300

    section form

--- src/route.js
+++ src/route.js
@@ -6,4 +6,7 @@ route.get('/', (req, res) => res.render('index'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
 route.get('/room', (req, res) => res.render('room'))
 
+// Formato que o formulario de dentro da modal tem que passar a informação
+// route.post(' ')
+
 module.exports = route
