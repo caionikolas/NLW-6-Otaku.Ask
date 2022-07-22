682893170d64375886b05ca1ecafdd2865cfcba3
caionikolas <Caio.kanu@hotmail.com>
2022-07-22 01:09:14 -0300

    banco de dados

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,3 +1,11 @@
 module.exports = {
-  create() {}
+  create(req, res) {
+    let roomId = 123456
+
+    for (var i = 0; i < 6; i++) {
+      Math.floor(Math.random() * 10)
+    }
+
+    res.redirect(`/room/${roomId}`)
+  }
 }
