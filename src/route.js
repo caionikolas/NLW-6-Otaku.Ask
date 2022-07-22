4d185ef1fe6d7378008ce70b826d40d4ec2bc8af
caionikolas <Caio.kanu@hotmail.com>
2022-07-21 23:46:39 -0300

    bug fix

--- src/route.js
+++ src/route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
@@ -7,7 +8,7 @@ route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
 route.get('/create-pass', (req, res) =>
   res.render('index', { page: 'create-pass' })
 )
-route.get('/room', (req, res) => res.render('room'))
+route.get('/room/:room', (req, res) => res.render('room'))
 
 // Formato que o formulario de dentro da modal tem que passar a informação
 route.post('/room/:room/:question/:action', QuestionController.index)
